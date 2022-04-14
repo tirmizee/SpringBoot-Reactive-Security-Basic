a739bec53345d1edf3899aec2f6134878b90e64f
Pratya Yeekhaday <pratya.yeekhaday@accenture.com>
2022-04-15 06:41:11 +0700

    revise code

--- src/main/java/com/tirmizee/SpringBootReactiveSecurityBasicApplication.java
+++ src/main/java/com/tirmizee/SpringBootReactiveSecurityBasicApplication.java
@@ -2,11 +2,10 @@ package com.tirmizee;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.boot.autoconfigure.web.reactive.WebFluxProperties;
 
 @SpringBootApplication
 public class SpringBootReactiveSecurityBasicApplication {
-	WebFluxProperties s;
+
 	public static void main(String[] args) {
 		SpringApplication.run(SpringBootReactiveSecurityBasicApplication.class, args);
 	}
